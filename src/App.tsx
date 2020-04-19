a9daea5cd67a784a445ab244574fb9b2a9fe1453
Kai Carver <kaicarver@gmail.com>
2020-04-19 13:54:26 +0200

    simple example success, remain underwhelmed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from './FirstComponent'
 
 function App() {
   return (
@@ -9,40 +10,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          It's React, using Typescript!
         </p>
-        <p>
-          It's Typescript!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <FirstComponent />
       </header>
-      <main>
-        <Home />
-      </main>
     </div>
   );
 }
 
-class Home extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-      this.state = {};
-    }
-    render(): JSX.Element | null {
-      return null;
-    }
-}
-interface Props {
-  readonly user: User
-}
-
-interface State {}
 
 export default App;
